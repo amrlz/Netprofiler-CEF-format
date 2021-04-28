7579980683c2810d38dcae122149a5c21aad06ef
amrlz <60924930+amrlz@users.noreply.github.com>
2021-04-28 10:03:07 +0100

    Update pysyslog.py

--- pysyslog.py
+++ pysyslog.py
@@ -1,12 +1,4 @@
 #!/usr/bin/env python
-"""
-Tiny Syslog Server in Python.
-
-This is a tiny syslog server that is able to receive UDP based syslog
-entries on a specified port and save them to a file.
-That's it... it does nothing else...
-There are a few configuration parameters.
-"""
 import socket
 import logging
 import socketserver
